5c7a88aecfc970f52cc1e0f3daa3f3119dfb2faa
Xanthorrhizol <xanthorrhizol@proton.me>
2025-07-21 21:47:28 +0900

    R: divide ActorSystem-related parts from actor.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod actor;
+pub mod actor_system;
 mod error;
 #[cfg(test)]
 mod test;
@@ -7,6 +8,7 @@ pub mod types;
 pub use crate::error::ActorError;
 pub use crate::types::{JobSpec, Message};
 pub use actor::*;
+pub use actor_system::*;
 
 #[macro_use]
 extern crate log;
