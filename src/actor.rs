afbea694b0d3e645755fe9ffe84d8c4ac47c8406
Xanthorrhizol <xanthorrhizol@proton.me>
2025-08-02 16:08:08 +0900

    D: comment on actor.rs

--- src/actor.rs
+++ src/actor.rs
@@ -2,28 +2,49 @@ use crate::{ActorError, ActorSystem, ActorSystemCmd, LifeCycle, Message};
 use std::error::Error;
 
 #[async_trait::async_trait]
+/// Trait for actors in the actor system
+/// An actor is a fundamental unit of computation that encapsulates state and behavior.
+/// Actors communicate with each other by sending messages, and they can be created, restarted, or stopped by the actor system.
+/// Actors can handle messages asynchronously and can be in different states (e.g., starting, receiving, stopping, restarting, terminated).
+/// Actors can also implement pre-start, pre-restart, post-stop, and post-restart hooks to perform actions at different lifecycle stages.
+/// Actors must implement the `actor` method to handle incoming messages and return results.
+/// Actors must also implement the `address` method to provide a unique identifier for the actor.
+/// Actors can be registered with the actor system using the `register` method, which will start the actor and handle its lifecycle.
 pub trait Actor
 where
     Self: Sized + 'static,
 {
+    /// The message type that the actor can handle.
     type Message: Sized + Send + serde::Serialize + serde::de::DeserializeOwned;
 
+    /// The result type that the actor returns after processing a message.
     type Result: Sized + Send + serde::Serialize + serde::de::DeserializeOwned;
 
+    /// The error type that the actor can return.
     type Error: Error + Send;
 
+    /// The address of the actor, which is a unique identifier for the actor in the actor system.
     fn address(&self) -> &str;
 
+    /// Handles incoming messages sent to the actor.
     async fn actor(&mut self, msg: Self::Message) -> Result<Self::Result, Self::Error>;
 
+    /// Pre-start hook that is called before the actor starts processing messages.
     async fn pre_start(&mut self) {}
 
+    /// Pre-restart hook that is called before the actor restarts.
     async fn pre_restart(&mut self) {}
 
+    /// Post-stop hook that is called after the actor stops processing messages.
     async fn post_stop(&mut self) {}
 
+    /// Post-restart hook that is called after the actor restarts.
     async fn post_restart(&mut self) {}
 
+    /// Registers the actor with the actor system and starts it.
+    /// This method will run the actor in a loop, handling messages and managing the actor's lifecycle.
+    /// > Don't implement this method directly.
+    /// > Instead, use the `register` method to register the actor with the actor system.
     async fn run_actor(
         &mut self,
         actor_system_tx: tokio::sync::mpsc::UnboundedSender<ActorSystemCmd>,
@@ -142,6 +163,8 @@ where
             ));
         }
     }
+
+    /// Registers the actor with the actor system and starts it.
     async fn register(
         mut self,
         actor_system: &mut ActorSystem,
